169d7b38f878c73bb55eff6b2ecb2896ac1ced6d
Yunosuke Kameoka <youzhijieg@gmail.com>
2023-11-24 13:57:24 +0900

    Change talker and listener to Person typ

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,17 +1,16 @@
-import rclpy                     
-from rclpy.node import Node      
-from std_msgs.msg import Int16   
+import rclpy
+from rclpy.node import Node
+from person_msgs.msg import Person
 
 rclpy.init()
-node = Node("talker")            
-pub = node.create_publisher(Int16, "countup", 10)
-
+node = Node("talker")
+pub = node.create_publisher(Person, "person", 10)
 n = 0
-
 def cb():
     global n
-    msg = Int16()
-    msg.data = n
+    msg = Person()         
+    msg.name = "YunosukeKameoka"  #msgファイルに書いた「name」
+    msg.age = 20            #msgファイルに書いた「age」
     pub.publish(msg)
     n += 1
 
